b1c0a29b9369d4cd9cf4febdfca8fa078852c852
kid84 <84takuya@gmail.com>
2014-10-11 14:18:56 +0900

    jade

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
  
 app.get('/', function(req, res) {
-  res.send('hello world');
+  // res.send('hello world');
+  res.render('index');
 });
+
  
 app.listen(3000);
 
